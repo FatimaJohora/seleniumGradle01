8df511b865287cb9782ed888244253452fe450d8
parufatima <122179298+parufatima@users.noreply.github.com>
2023-03-22 07:13:04 +0100

    1 change

--- src/test/java/FirefoxBrowserLaunch.java
+++ src/test/java/FirefoxBrowserLaunch.java
@@ -1,7 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Test;
 import java.time.Duration;
 
